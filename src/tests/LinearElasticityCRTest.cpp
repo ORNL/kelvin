d7d55b196a5cdae7205cc8a81b0f4dd6863a50fb
Jay Jay Billings <billingsjj@ornl.gov>
2018-10-10 11:02:57 -0400

    Added linear elasticity stubs.
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

---
+++ src/tests/LinearElasticityCRTest.cpp
@@ -0,0 +1,12 @@
+/*
+ * LinearElasticityCRTest.cpp
+ *
+ *  Created on: Oct 5, 2018
+ *      Author: bkj
+ */
+
+#include "/home/bkj/git/furnace-prototype/src/LinearElasticityCR.h"
+
+namespace Kelvin {
+
+} /* namespace Kelvin */
