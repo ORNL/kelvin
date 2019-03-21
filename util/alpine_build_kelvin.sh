b2380c5e859b5662b41ca215755a92d170f60fdb
Jay Jay Billings <billingsjj@ornl.gov>
2019-03-21 16:12:10 -0400

    Set the default build to Release mode in the install scripts.
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

--- util/alpine_build_kelvin.sh
+++ util/alpine_build_kelvin.sh
@@ -4,7 +4,7 @@
 echo $PWD
 mkdir Kelvin/build
 cd Kelvin/build/
-cmake ../ -DPARSERS_DIR=/usr/local -DMFEM_DIR=/usr/local
+cmake ../ -DPARSERS_DIR=/usr/local -DMFEM_DIR=/usr/local -DCMAKE_BUILD_TYPE=Release
 make -j4
 make test
 # Exit with the status of the test command
--- util/rhel/rhel_build_kelvin.sh
+++ util/rhel/rhel_build_kelvin.sh
@@ -5,7 +5,7 @@
 
 # Install Kelvin
 cd $1
-cmake ../ -DPARSERS_DIR=$2 -DMFEM_DIR=$2 -DCMAKE_INSTALL_PREFIX=$2 -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.5
+cmake ../ -DPARSERS_DIR=$2 -DMFEM_DIR=$2 -DCMAKE_INSTALL_PREFIX=$2 -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.5 -DCMAKE_BUILD_TYPE=Release
 make -j5
 make test
 make install
